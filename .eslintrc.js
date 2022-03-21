bd6d446005011fdf87a60ccabe30438ca589911a
tak001 <twitwitwi2@yahoo.co.jp>
2022-03-21 16:57:32 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'no-useless-constructor': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'no-use-before-define': 'off',
+    'max-len': ['error', { code: 80, ignoreUrls: true }],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+  },
+};
